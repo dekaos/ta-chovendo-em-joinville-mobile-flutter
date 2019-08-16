a7b18131cdd862c35df6a23ad284ab5e68177d9f
Anderson Stühler <anderson@stuhler.com.br>
2019-08-16 17:41:24 -0300

    WIP - Tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ta_chovendo_em_joinville_m/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(JoinvilleRains());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
