b648db935de63f864f58c45bfd61a3ebf382cd0b
Anderson Stühler <anderson@stuhler.com.br>
2019-08-19 14:46:19 -0300

    A simple method do show error messages by code

--- lib/helpers/handle_exception.dart
+++ lib/helpers/handle_exception.dart
@@ -1,4 +1,15 @@
 class HandleException implements Exception {
   String exception;
   HandleException(this.exception);
+
+  String errorMessageByCode (int code) {
+    Map status = Map();
+    status[400] = 'Erro ao processar requisição.';
+    status[401] = 'Usuário não autorizado.';
+    status[404] = 'Cidade não encontrada, tente uma nova busca.';
+    status[500] = 'Erro interno do servidor.';
+    status[503] = 'Serviço indisponível.';
+
+    return status[code];
+  }
 }
